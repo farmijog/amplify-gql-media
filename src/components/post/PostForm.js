3090a91389f783d4766e19d8e724a1f96fecd506
Felipe <felipe.armijo05@hotmail.com>
2021-02-20 18:18:23 -0300

    Image upload fixes

--- src/components/post/PostForm.js
+++ src/components/post/PostForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Button, TextField } from "@material-ui/core";
 import { API, graphqlOperation, Storage } from "aws-amplify";
-import { CircularProgress as Loading } from "@material-ui/core";
+import { Button, CircularProgress as Loading, IconButton, TextField } from "@material-ui/core";
+import { ImageSearch } from "@material-ui/icons";
 
 import * as mutations from "../../graphql/mutations";
 
 function PostForm() {
+    const [imgFile, setImgFile] = useState(null);
     const [postData, setPostData] = useState({ message: "" });
     const [postLoading, setPostLoading] = useState(false);
 
@@ -17,15 +18,17 @@ function PostForm() {
         if (!e.target.files[0]) return;
         const file = e.target.files[0];
         setPostData({ ...postData, image: file.name });
-        await Storage.put(file.name, file);
+        setImgFile(file);
     }
 
     const createPost = async () => {
-        const { message } = postData;
-        if (message === "") return;
         try {
             setPostLoading(true)
             await API.graphql(graphqlOperation(mutations.createPost, { input: postData }))
+            if (imgFile !== null) {
+                await Storage.put(postData.image, imgFile)
+                setImgFile(null);
+            }
             setPostData({ message: "" });
         } catch (error) {
             console.log("Error while trying to post: ", error);
@@ -46,21 +49,27 @@ function PostForm() {
                     multiline
                     rowsMax={5}
                 />
-                <input type="file" onChange={onChangeImg} />
-                <br/>
+                <IconButton component="label">
+                    <ImageSearch />
+                    <input type="file" accept="image/*" onChange={onChangeImg} hidden />
+                </IconButton>
+                {/* <Button variant="contained" size="small" style={{ textTransform: "none" }}
+                    onClick={() => console.log("postData: ", postData, "imgFile: ", imgFile)}
+                >
+                    test log
+                </Button> */}
                 {postLoading ? (
                     <Loading style={{ float: "right" }} />        
                 ) : (
-                    
-                    <Button 
+                    <Button
+                    disabled={postData.message.trim() === "" && !postData.image ? true : false}
                     onClick={createPost}
                     variant="contained" 
                     color="primary" 
-                    style={{ float: "right", borderRadius: 50, textTransform: "none", marginTop: 5 }}
+                    style={{ float: "right", borderRadius: 50, textTransform: "none", marginTop: 10 }}
                 >
                     Post
                 </Button>
-                    
                 )}
             </div>
         </div>
